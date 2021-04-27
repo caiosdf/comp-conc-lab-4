b53bdd49a1759dcebe96534a53d736690ac98df6
Caio <caiosdf15@gmail.com>
2021-04-26 23:02:23 -0300

    ajustes

--- hellobye.c
+++ hellobye.c
@@ -3,13 +3,13 @@
 /* Laboratório: 4 */
 /* Codigo: Uso de variáveis de condição e suas operações básicas para sincronização por condição */
 
-/***** Condicao logica da aplicacao: as threads A so podem imprimir "Bye" depois que a thread B imprimir  "Hello"  ****/
+/***** Condicao logica da aplicacao: a thread B so pode imprimir "Bye" depois que duas threads A imprimirem  "Hello"  ****/
 
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NTHREADS  4
+#define NTHREADS  3
 
 /* Variaveis globais */
 int x = 0;
@@ -18,48 +18,38 @@ pthread_cond_t x_cond;
 
 /* Thread A */
 void *A (void *t) {
-  long int id = (long int) t;
+  int boba1, boba2;
 
-  printf("Seja bem-vindo!\n");
+  printf("A: Comecei\n");
+  
+  /* faz alguma coisa pra gastar tempo... */
+  boba1=10000; boba2=-10000; while (boba2 < boba1) boba2++;
+  
+  printf("HELLO\n");
 
   pthread_mutex_lock(&x_mutex);
   x++;
-  pthread_cond_broadcast(&x_cond);
+  if (x==2) {
+      printf("A:  x = %d, vai sinalizar a condicao \n", x);
+      pthread_cond_signal(&x_cond);
+  }
   pthread_mutex_unlock(&x_mutex);
+
   pthread_exit(NULL);
 }
 
 /* Thread B */
 void *B (void *t) {
-  long int id = (long int) t;
+  printf("B: Comecei\n");
 
   pthread_mutex_lock(&x_mutex);
-  if (x < 1) { 
+  if (x < 2) { 
+     printf("B: x= %d, vai se bloquear...\n", x);
      pthread_cond_wait(&x_cond, &x_mutex);
+     printf("B: sinal recebido e mutex realocado, x = %d\n", x);
   }
-  pthread_mutex_unlock(&x_mutex);
-  if(id == 1){
-    x++; 
-    printf("Fique a vontade.\n");
-  }
-  if(id == 2){
-    x++; 
-    printf("Sente-se por favor.\n");
-  } 
-  if(x == 3) pthread_cond_broadcast(&x_cond);
-  pthread_exit(NULL);
-}
-
-/* Thread C */
-void *C (void *t) {
-  long int id = (long int) t;
-
-  pthread_mutex_lock(&x_mutex);
-  if (x < 3) { 
-     pthread_cond_wait(&x_cond, &x_mutex);
-  }
-  pthread_mutex_unlock(&x_mutex);
-  printf("Volte sempre!\n");
+  printf("BYE\n");
+  pthread_mutex_unlock(&x_mutex); 
   pthread_exit(NULL);
 }
 
@@ -67,17 +57,15 @@ void *C (void *t) {
 int main(int argc, char *argv[]) {
   int i; 
   pthread_t threads[NTHREADS];
-  long int id1 = 0, id2 = 1, id3 = 2, id4 = 3;
 
   /* Inicilaiza o mutex (lock de exclusao mutua) e a variavel de condicao */
   pthread_mutex_init(&x_mutex, NULL);
   pthread_cond_init (&x_cond, NULL);
 
   /* Cria as threads */
-  pthread_create(&threads[0], NULL, A, (void *) id1);
-  pthread_create(&threads[1], NULL, B, (void *) id2);
-  pthread_create(&threads[2], NULL, B, (void *) id3);
-  pthread_create(&threads[3], NULL, C, (void *) id4);
+  pthread_create(&threads[2], NULL, A, NULL);
+  pthread_create(&threads[1], NULL, A, NULL);
+  pthread_create(&threads[0], NULL, B, NULL);
 
   /* Espera todas as threads completarem */
   for (i = 0; i < NTHREADS; i++) {
@@ -88,5 +76,4 @@ int main(int argc, char *argv[]) {
   /* Desaloca variaveis e termina */
   pthread_mutex_destroy(&x_mutex);
   pthread_cond_destroy(&x_cond);
-  return 0;
 }
